f2fd026ecfe39f99013a22b4a3d3fba1abc608ee
Timelement <timeexe@gmail.com>
2020-07-24 16:01:27 -0700

    First pass at enrty styling

--- diary-app/src/components/Entry.jsx
+++ diary-app/src/components/Entry.jsx
@@ -8,22 +8,35 @@ const Container = styled.div`
 	display: flex;
 	flex: 1;
 	flex-direction: column;
-	background-color: blue;
+	align-items: center;
+	background-color: #4f5f76;
+	font-family: "Lucida Console", Monaco, monospace;
+
 
 	.date {
-		color: orange;
+		padding: 0.25em;
+		color:  #e5eaf5;
 
 	}
 
 	.content {
-		background-color: gray;
-		font-family: Impact, Charcoal, sans-serif;
+		color: #e5eaf5;
+		background-color: #091f36;
+		padding: 2.0em;
+		width: 75%;
+		margin: 25px;
+		border-radius: 1.563em;
+	    
 
 	}
 
 	.title { 
-		font-size: 0.75vw;
-		color: silver;
+		padding: 1.563em;
+		font-size: 1.0	em, 1.25em, 0, 1.25em;
+		color: #0f2862;
+		text-transform: uppercase;
+		font-weight; 800;
+		text-shadow: 5px 5px 10px #e5eaf5;
 
 	}
 
@@ -35,9 +48,14 @@ const Entry = ({id, data}) => {
 	return (
 		<Container>
 		
+		<div className="title"><h2>{data.title}</h2></div>
 		<div className="date">{data.date}</div>
-		<div className="content">{data.entry}</div>
-		<div className="title">{data.title}</div>
+		<div className="content">
+
+		<div id="">{data.entry}</div>
+
+		</div>
+		
 
 		</Container>
 		);
