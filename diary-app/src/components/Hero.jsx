194d24f2bbc663668c4a93c6b298a881680480e2
Eternal Vigil <eternalvigil@pop-os.localdomain>
2020-07-26 14:47:25 -0700

    adjusted spacing, removed empty object

--- diary-app/src/components/Hero.jsx
+++ diary-app/src/components/Hero.jsx
@@ -3,7 +3,6 @@ import React, {useEffect, useState} from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-
 .hero-image {
     width: 100%;
 }
@@ -12,15 +11,13 @@ const Container = styled.div`
 //Declared Components
 
 const Hero = ({}) => {
-
     const heroUrl = "https://images.unsplash.com/photo-1586184028447-9e913a92e8ba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2978&q=80";
 
-    return (
+    return(
         <Container>
             <img className="hero-image" src={heroUrl}/>
         </Container>
-
-        );
+    );
 };
 
 //export
